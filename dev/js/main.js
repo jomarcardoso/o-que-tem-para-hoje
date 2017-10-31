b57eb6d7ec019a24a8883a041d51542336f554fe
Jomar Antonio Cardoso <jomar.cardoso@cwi.com.br>
2017-10-31 15:42:46 -0200

    fita

--- dev/js/main.js
+++ dev/js/main.js
@@ -1,8 +1,22 @@
-var request = new RequestService();
+// var request = new RequestService();
+//
+// function testRequestApi() {
+//     request.get('/arezzo-api/function-test-api')
+//     .done(function(data) {
+//       console.log(data);
+//     });
+// }
 
-function testRequestApi() {
-    request.get('/arezzo-api/function-test-api')
-    .done(function(data) {
-      console.log(data);
-    });
-}
+
+var scrollerTopMargin = $("#fita").offset().top;
+$(window).scroll(function(){
+    var c = $(window).scrollTop();
+    var d = $("#fita");
+    if (c > scrollerTopMargin) {
+        d.css({ position: "fixed", top: "0px", left: "0px", right: "0px" });
+    }
+    else if (c <= scrollerTopMargin)
+    {
+        d.css({ position: "relative", top: ""   });
+    }
+});
